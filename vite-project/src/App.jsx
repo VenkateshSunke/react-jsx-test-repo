0d109fa0627155705ea34b07ca472f4c2268e0df
venkatesh.sunke <venkatesh.sunke@onelabventures.com>
2025-11-07 17:30:35 +0530

    intial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import Hero from "./components/Hero";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial", padding: 20 }}>
+      <h1>Welcome to My Personal Website</h1>
+      <Hero />
+    </div>
+  );
+}
